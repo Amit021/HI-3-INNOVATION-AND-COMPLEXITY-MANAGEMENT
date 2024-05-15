6d453ee396e508628ded47323c27aba38e2349fe
Amit021 <d.arghya93@gmail.com>
2024-05-15 19:29:44 +0200

    Docker Updated

--- setup.sh
+++ setup.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status
-set -e
-
-# Run database migrations
-echo "Running migrations..."
-python manage.py migrate
-
-# Start the Django development server
-echo "Starting the development server..."
 python manage.py runserver 0.0.0.0:8000
